f9d31db459fa8eab5ff52d0ebc13dc15882d524d
Reyaansh <reyaanshvohra@gmail.com>
2026-03-22 21:07:32 -0300

    Done
    Tell me if I should change or add anything

---
+++ level-4/game.gd
@@ -0,0 +1,76 @@
+extends Node2D
+
+var time_left = 90
+var meteors_destroyed = 0
+var total_meteors = 12
+var game_started = false
+
+func _ready():
+	$UI/StartLabel.text = _get_start_text()
+	$UI/StartLabel.show()
+	$UI/Msg.hide()
+	$UI/Timer.hide()
+	$UI/Ammo.hide()
+	get_tree().paused = true
+
+func _get_start_text() -> String:
+	var player = $Player
+	return """GET READY!
+
+Time limit: %d seconds
+Meteors to destroy: %d
+Bullets per magazine: %d
+Time between shots: %.1f seconds
+Reload time: %.1f seconds
+
+Press SPACE to Start""" % [time_left, total_meteors, player.magazine_size, player.shoot_cooldown, player.reload_time]
+
+func start_game():
+	if game_started:
+		return
+	game_started = true
+	$UI/StartLabel.hide()
+	$UI/Timer.show()
+	$UI/Ammo.show()
+	update_ui()
+	$Timer.start()
+	$MeteorSpawn.start_spawning()
+	get_tree().paused = false
+
+func meteor_destroyed():
+	meteors_destroyed += 1
+	
+	if meteors_destroyed >= total_meteors:
+		victory()
+
+func game_over(msg):
+	$UI/Msg.text = msg + "\nPress R to Restart"
+	$UI/Msg.show()
+	$UI/Timer.hide()
+	$UI/Ammo.hide()
+	get_tree().paused = true
+
+func victory():
+	$UI/Msg.text = "YOU WIN!\n"
+	$UI/Msg.show()
+	$UI/Timer.hide()
+	$UI/Ammo.hide()
+	get_tree().paused = true
+
+func update_ui():
+	$UI/Timer.text = "Time: " + str(time_left)
+
+func _input(event):
+	if not game_started:
+		if Input.is_action_just_pressed("shoot") or Input.is_action_just_pressed("ui_accept"):
+			start_game()
+		return
+	if Input.is_action_just_pressed("restart"):
+		get_tree().paused = false
+		get_tree().reload_current_scene()
+
+func _on_timer_timeout() -> void:
+	time_left -= 1
+	update_ui()
+	if time_left <= 0:
+		game_over("Time's up!")
---
+++ level-4/player.gd
@@ -0,0 +1,75 @@
+extends CharacterBody2D
+
+@export var speed = 300
+
+var magazine_size = 7
+var bullets_left = 7
+var shoot_cooldown = 2.0
+var reload_time = 10.0
+
+var can_shoot = true
+var reloading = false
+
+func _physics_process(delta):
+	var direction = Vector2.ZERO
+	
+	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	velocity = direction.normalized() * speed
+	move_and_slide()
+	
+	var screen_size = get_viewport_rect().size
+	var offset = Vector2(124, 128)
+	var radius = 87.0
+	var min_pos = Vector2(radius - offset.x, radius - offset.y)
+	var max_pos = Vector2(screen_size.x - offset.x - radius, screen_size.y - offset.y - radius)
+	position.x = clampf(position.x, min_pos.x, max_pos.x)
+	position.y = clampf(position.y, min_pos.y, max_pos.y)
+	
+	check_collision()
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func shoot():
+	if not can_shoot or reloading:
+		return
+	
+	if bullets_left <= 0:
+		reload()
+		return
+	
+	bullets_left -= 1
+	
+	var bullet = preload("res://Bullet.tscn").instantiate()
+	bullet.global_position = $Marker2D.global_position
+	
+	get_tree().current_scene.add_child(bullet)
+	
+	can_shoot = false
+	await get_tree().create_timer(shoot_cooldown).timeout
+	can_shoot = true
+	
+	if bullets_left == 0:
+		reload()
+
+func reload():
+	reloading = true
+	await get_tree().create_timer(reload_time).timeout
+	bullets_left = magazine_size
+	reloading = false
+
+func die():
+	hide()
+	set_physics_process(false)
+	set_process(false)
+	get_tree().current_scene.game_over("You got hit!")
+
+func check_collision():
+	for i in range(get_slide_collision_count()):
+		var coll = get_slide_collision(i)
+		var body = coll.get_collider()
+		if body.is_in_group("meteor"):
+			die()
