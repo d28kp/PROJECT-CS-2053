783d3609b4d33ab7df008c623deeefa1aef66744
Apoorva Christraj Devi <155484119+Apoorva-CD@users.noreply.github.com>
2026-03-30 19:38:10 -0300

    Delete level1 directory

--- level1/end.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _ready():
-	$MenuButton.pressed.connect(_on_menu_pressed)
-
-func _on_menu_pressed():
-	get_tree().change_scene_to_file("res://menu.tscn")
--- level1/menu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _ready():
-	$LEVEL1.pressed.connect(_on_start_pressed)
-
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://level_1.tscn")
